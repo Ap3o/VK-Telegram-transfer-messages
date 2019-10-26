5445e7368da9d71fd02ffdf29bd9e68d99fd27dc
NeoD <flesakov.nik09@gmail.com>
2019-10-26 21:55:23 +0500

    Добавлен удобный метод отправки сообщения

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 import sqlite3
-from datetime import datetime
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll
-
+import random
 import config
+
+items = list(range(9999))
+
+
+def shuffle(items=items):
+    random.shuffle(items)
+    array = items[:1]
+    return array
 
 
 class VK(object):
@@ -12,9 +19,8 @@ class VK(object):
         self.session_api = self.session.get_api()
         self.LongPoll = VkBotLongPoll(self.session, id)
 
-    # TODO
-    def send_msg(self):
-        pass
+    def send_msg(self, peer_id, text):
+        self.session_api.messages.send(peer_id=peer_id, message=text, random_id=shuffle())
 
 
 VkBot = VK(config.vk_token, config.vk_group_id)
